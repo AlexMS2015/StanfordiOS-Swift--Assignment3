6b8d7799f92aae4e329f67be338ef3032dd6d0c5
AlexMS2015 <Alex@192-168-1-7.tpgi.com.au>
2016-05-07 15:35:37 +1000

    Working the same as the in class version

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,11 +12,70 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
     
-    @IBAction func appendDigit(sender: AnyObject) {
-        
-        
+    var currentlyTypingNumber = false;
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if currentlyTypingNumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            currentlyTypingNumber = true;
+        }
     }
     
+    
+    @IBAction func operate(sender: UIButton) {
+        if currentlyTypingNumber {
+            enter()
+        }
+        
+        let operation = sender.currentTitle!
+        
+        switch operation {
+        case "*": performOperation({ return $0 * $1 })
+        case "/": performOperation({ return $1 / $0 })
+        case "+": performOperation({ return $0 + $1 })
+        case "-": performOperation({ return $1 - $0 })
+        case "sq": performOperation({ return sqrt($0) })
+        default: break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            operandStack.append(displayValue)
+            print(operandStack)
+        }
+    }
+
+    @nonobjc
+    func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            operandStack.append(displayValue)
+            print(operandStack)
+        }
+    }
+    
+    var operandStack = Array<Double>()
+    @IBAction func enter() {
+        operandStack.append(displayValue)
+        currentlyTypingNumber = false;
+        print(operandStack)
+    }
+    
+    var displayValue : Double {
+        get {
+            return Double(display.text!)!
+        }
+        
+        set {
+            display.text = "\(newValue)"
+        }
+        
+    }
 
 }
 
